2a771eef0007637c7b676acbcba56ac2b0e6876e
Sergio Chan <yuheng9211@qq.com>
2016-08-05 22:06:48 +0800

    update

--- pi_surveillance_1.py
+++ pi_surveillance_1.py
@@ -13,26 +13,19 @@ import json
 import time
 import cv2
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--conf", required=True,
-	help="path to the JSON configuration file")
-args = vars(ap.parse_args())
-
 # filter warnings, load the configuration and initialize the Dropbox
 warnings.filterwarnings("ignore")
-conf = json.load(open(args["conf"]))
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = tuple(conf["resolution"])
-camera.framerate = conf["fps"]
-rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
+camera.resolution = tuple([640,480])
+camera.framerate = 16
+rawCapture = PiRGBArray(camera, size=tuple([640,480]))
 
 # allow the camera to warmup, then initialize the average frame, last
 # uploaded timestamp, and frame motion counter
 print "[INFO] warming up..."
-time.sleep(conf["camera_warmup_time"])
+time.sleep(2,5)
 avg = None
 lastUploaded = datetime.datetime.now()
 motionCounter = 0
@@ -65,7 +58,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 
 	# threshold the delta image, dilate the thresholded image to fill
 	# in holes, then find contours on thresholded image
-	thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255,
+	thresh = cv2.threshold(frameDelta, 5, 255,
 		cv2.THRESH_BINARY)[1]
 	thresh = cv2.dilate(thresh, None, iterations=2)
 	(cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
@@ -74,7 +67,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 	# loop over the contours
 	for c in cnts:
 		# if the contour is too small, ignore it
-		if cv2.contourArea(c) < conf["min_area"]:
+		if cv2.contourArea(c) < 5000:
 			continue
 
 		# compute the bounding box for the contour, draw it on the frame,
@@ -93,13 +86,13 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 	# check to see if the room is occupied
 	if text == "Occupied":
 		# check to see if enough time has passed between uploads
-		if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
+		if (timestamp - lastUploaded).seconds >= 3.0:
 			# increment the motion counter
 			motionCounter += 1
 
 			# check to see if the number of frames with consistent motion is
 			# high enough
-			if motionCounter >= conf["min_motion_frames"]:
+			if motionCounter >= 8:
 				# update the last uploaded timestamp and reset the motion
 				# counter
 				lastUploaded = timestamp
@@ -110,7 +103,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 		motionCounter = 0
 
 	# check to see if the frames should be displayed to screen
-	if conf["show_video"]:
+	if True:
 		# display the security feed
 		cv2.imshow("Security Feed", frame)
 		key = cv2.waitKey(1) & 0xFF
